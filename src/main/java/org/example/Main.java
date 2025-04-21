4e38862bd455cc725e56306775cea2e9c1b25131
Ian Gorton <gortonator@users.noreply.github.com>
2025-04-22 09:57:35 +1000

    fixed imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,10 +12,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
